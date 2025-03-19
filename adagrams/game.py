a7944887b5576183e20de321bfc813a5b0dd39ff
Dennif <dennif.pa@gmail.com>
2025-03-19 14:53:07 -0700

    wave 3 and 4 added

--- adagrams/game.py
+++ adagrams/game.py
@@ -59,7 +59,42 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    letter_score = {
+        'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 
+        'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+        'D': 2, 'G': 2,
+        'B': 3, 'C': 3, 'M': 3, 'P': 3,
+        'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+        'K': 5,
+        'J': 8, 'X': 8,
+        'Q': 10, 'Z': 10
+    }
+    word = word.upper()
+    total_points = 0 
+    for letter in word:
+        total_points += letter_score[letter]
+    if len(word) >=7:
+        total_points += 8
+    return total_points
 
 def get_highest_word_score(word_list):
-    pass
+    highest_score = 0 
+    best_word = ""
+    
+    for word in word_list:
+        score = score_word(word)
+        
+        if score > highest_score:
+            best_word = word
+            highest_score = score
+                
+        elif score == highest_score:
+            if len(word) == 10 and len(best_word) != 10:
+                best_word = word
+                highest_score = score
+                
+            elif len(word) < len(best_word) and len(best_word) != 10:
+                best_word = word
+                highest_score = score
+
+    return (best_word, highest_score)
